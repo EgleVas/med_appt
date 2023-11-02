c59f41a81f9f4b8eb5bafd96884073b27a4878dc
EgleVas <egle.vasileviciute@gmail.com>
2023-11-02 05:58:14 -0400

    add instant consultation booking

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Navbar/Navbar.js';
 import LandingPage from './Components/Landing_Page/LandingPage.js';
 import Login from './Components/Login/Login.js';
 import SignUp from './Components/Sign_Up/Sign_Up.js';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation.js';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/" element={<LandingPage />}/>
                 <Route path="/Login" element={<Login />}/>
                 <Route path="/SignUp" element={<SignUp />}/>
+                <Route path="/instant-consultation" element={<InstantConsultation />} />
               </Routes>
             
         </BrowserRouter>
